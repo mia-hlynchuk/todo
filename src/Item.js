ef738704a717ccafd701780208acbbb5b61cf662
mia-hlynchuk <solomiya.hlynchuk@gmail.com>
2017-10-21 02:49:18 -0400

    Added a checkbox for the Item component

--- src/Item.js
+++ src/Item.js
@@ -4,12 +4,31 @@ import './Item.css';
 class Item extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      checked: false
+    };
+    this.checkOrUncheck = this.checkOrUncheck.bind(this);
+  }
+
+  checkOrUncheck() {
+    // check if the item is already checked or not
+    if(this.state.checked) {
+      this.setState({
+        checked: false
+      });
+    } else {
+      this.setState({
+        checked: true
+      });
+    }
   }
 
   render() {
     console.log(this.props.text);
     return (
-      <li> {this.props.text} </li>
+      <li className={this.state.checked ? 'checked' : null}> 
+        <label><input type="checkbox" onChange={this.checkOrUncheck}/>{this.props.text}</label>
+      </li>
     );
   }
 }
